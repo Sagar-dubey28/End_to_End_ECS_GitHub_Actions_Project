46892b47ea6d5fa4b74a504eb3caba8fc42614ca
Sagar <rohit28sagar@gmail.com>
2026-09-22 13:37:40 +0530

    Comment out SNS topic subscription

--- terraform/main.tf
+++ terraform/main.tf
@@ -264,11 +264,11 @@ resource "aws_sns_topic" "alerts" {
   name = "${local.name}-alerts"
 }
 
-resource "aws_sns_topic_subscription" "email" {
-  topic_arn = aws_sns_topic.alerts.arn
-  protocol  = "email"
-  endpoint  = var.alert_email
-}
+# resource "aws_sns_topic_subscription" "email" {
+#   topic_arn = aws_sns_topic.alerts.arn
+#   protocol  = "email"
+#   endpoint  = var.alert_email
+# }
 
 resource "aws_cloudwatch_metric_alarm" "high_cpu" {
   alarm_name        = "${local.name}-high-cpu"
